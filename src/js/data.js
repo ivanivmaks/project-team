a5b47646c4b60aaab68a2563e39d9fe39708da56
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-26 18:50:20 +0300

    updated

--- src/js/data.js
+++ src/js/data.js
@@ -1,64 +1,24 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyCZOgZa27UP5HpyJ0isI9K27PBbA09iWzA',
-  authDomain: 'project-team-obd.firebaseapp.com',
-  projectId: 'project-team-obd',
-  storageBucket: 'project-team-obd.appspot.com',
-  messagingSenderId: '828835118965',
-  appId: '1:828835118965:web:e734c8b26ed4c1807f5d3c',
-  measurementId: 'G-81DT6E36X4',
-};
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const form = document.getElementById('hero-modal__form');
+const submitButton = document.getElementById('hero-modal-submit');
 
-const nameInput = document.getElementById('personal-name');
-const surnameInput = document.getElementById('personal-surname');
-const emailInput = document.getElementsByClassName('hero-modal__input-3')[0];
-const phoneInput = document.getElementsByClassName('hero-modal__input-tel')[0];
-const cardInput = document.getElementsByClassName('hero-modal__input-5')[0];
-const commentInput = document.getElementsByClassName('hero-modal__input-6')[0];
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const name = document.getElementById('personal-name');
+  const surname = document.getElementById('personal-surname');
+  const email = document.getElementById('user-mail');
+  const phoneNumber = document.getElementById('user-phone');
+  const cardNumber = document.getElementById('user-card');
+  const comment = document.getElementById('user-comment');
 
-console.log('fsfv');
-
-document
-  .getElementById('hero-modal__form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault(); // Зупинка стандартної поведінки форми
-
-    // Отримання значень з полів форми
-    const name = nameInput.value;
-    const surname = surnameInput.value;
-    const email = emailInput.value;
-    const phone = phoneInput.value;
-    const card = cardInput.value;
-    const comment = commentInput.value;
-
-    // Створення нового об'єкту з отриманими даними
-    const data = {
-      name: name,
-      surname: surname,
-      email: email,
-      phone: phone,
-      card: card,
-      comment: comment,
-    };
-
-    console.log(data);
-
-    // Надсилання даних до бази даних
-    const newEntryRef = database.ref().push();
-    newEntryRef
-      .set(data)
-      .then(() => {
-        console.log('Дані збережено');
-        // Очистка полей формы
-        nameInput.value = '';
-        surnameInput.value = '';
-        emailInput.value = '';
-        phoneInput.value = '';
-        cardInput.value = '';
-        commentInput.value = '';
-      })
-      .catch(error => {
-        console.error('Помилка', error);
-      });
-  });
+  const userData = {
+    name: name.value,
+    surname: surname.value,
+    email: email.value,
+    phoneNumber: phoneNumber.value,
+    cardNumber: cardNumber.value,
+    comment: comment.value,
+  };
+  console.log(userData);
+  localStorage.setItem('userData', JSON.stringify(userData));
+  form.reset();
+});
