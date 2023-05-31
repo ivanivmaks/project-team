db88a7f570aee420dea80bb30dfc4f751d4d4025
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-31 15:35:18 +0300

    updated

--- src/js/data.js
+++ src/js/data.js
@@ -22,3 +22,42 @@ form.addEventListener('submit', function (event) {
   localStorage.setItem('userData', JSON.stringify(userData));
   form.reset();
 });
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); // Зупиняємо стандартну поведінку форми
+
+  const formData = new FormData(form); // Отримуємо дані з форми
+
+  // Формуємо об'єкт з даними, які будемо відправляти на сервер
+  const data = {
+    name: formData.get('user-name'),
+    surname: formData.get('user-surname'),
+    email: formData.get('user_mail'),
+    phoneNumber: formData.get('user_phone'),
+    cardNumber: formData.get('user_card'),
+    comment: formData.get('user_name'),
+  };
+
+  // Використовуйте функцію fetch для відправки даних на сервер
+  fetch('/your-email-endpoint', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (response) {
+      if (response.ok) {
+        // Дані успішно відправлені
+        console.log('Data sent successfully');
+        // Тут ви можете додати код для підтвердження або сповіщення користувача
+      } else {
+        // Виникла помилка при відправці даних
+        console.log('Error sending data');
+        // Тут ви можете додати код для обробки помилки або сповіщення користувача
+      }
+    })
+    .catch(function (error) {
+      console.log('Error sending data:', error);
+    });
+});
