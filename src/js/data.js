9cc39b054bbc0b6cf6ba348f5cf86e327f1b8736
ivaniv_maks <ivanivmaks0@gmail.com>
2023-05-31 16:42:11 +0300

    updated

--- src/js/data.js
+++ src/js/data.js
@@ -1,24 +1,63 @@
+// const form = document.getElementById('hero-modal__form');
+// const submitButton = document.getElementById('hero-modal-submit');
+
+// form.addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   const name = document.getElementById('personal-name');
+//   const surname = document.getElementById('personal-surname');
+//   const email = document.getElementById('user-mail');
+//   const phoneNumber = document.getElementById('user-phone');
+//   const cardNumber = document.getElementById('user-card');
+//   const comment = document.getElementById('user-comment');
+
+//   const userData = {
+//     name: name.value,
+//     surname: surname.value,
+//     email: email.value,
+//     phoneNumber: phoneNumber.value,
+//     cardNumber: cardNumber.value,
+//     comment: comment.value,
+//   };
+//   console.log(userData);
+//   localStorage.setItem('userData', JSON.stringify(userData));
+//   form.reset();
+// });
+
 const form = document.getElementById('hero-modal__form');
-const submitButton = document.getElementById('hero-modal-submit');
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  const name = document.getElementById('personal-name');
-  const surname = document.getElementById('personal-surname');
-  const email = document.getElementById('user-mail');
-  const phoneNumber = document.getElementById('user-phone');
-  const cardNumber = document.getElementById('user-card');
-  const comment = document.getElementById('user-comment');
 
-  const userData = {
-    name: name.value,
-    surname: surname.value,
-    email: email.value,
-    phoneNumber: phoneNumber.value,
-    cardNumber: cardNumber.value,
-    comment: comment.value,
+  const formData = new FormData(form);
+
+  const data = {
+    name: formData.get('user-name'),
+    surname: formData.get('user-surname'),
+    email: formData.get('user_mail'),
+    phoneNumber: formData.get('user_phone'),
+    cardNumber: formData.get('user_card'),
+    comment: formData.get('user_name'),
   };
-  console.log(userData);
-  localStorage.setItem('userData', JSON.stringify(userData));
+
+  fetch('/ivanivmaks@gmail.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.log('Email sent successfully');
+        // Додаткові дії після успішної відправки листа
+      } else {
+        console.log('Error sending email');
+        // Обробка помилки відправки листа
+      }
+    })
+    .catch(function (error) {
+      console.log('Error sending email:', error);
+    });
+
   form.reset();
 });
